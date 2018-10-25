9c88c5270c58d0f0da48f61f39b3900c2a2f70b6
Evan Tolley <etolley@guthriegroup.com>
2018-10-24 20:33:39 -0400

    second commit

--- src/app/events/event-thumbnail-component.ts
+++ src/app/events/event-thumbnail-component.ts
@@ -1,14 +1,31 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core'
+import { Component, Input } from '@angular/core'
 
 @Component({
     selector : 'event-thumbnail',
     template: ` 
     <div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Price: \${{event.price}}</div>
+        <h2>{{event?.name}}</h2>
+        <div>Date: {{event?.date}}</div>
+        <div  [ngSwitch]="event?.time">Time: {{event?.time}}    
+        <span *ngSwitchCase="'8:00am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00am'">(Late Start)</span>
+        <span *ngSwitchDefault >(Normal Start)</span>
+        </div>
+      
+        <div>Price: \${{event?.price}}</div>
+        <div *ngIf="event?.location">
+            <span>Location: {{event?.location?.address}}</span>
+            <span class="pad-left"></span>
+            <span>{{event?.location?.city}}, {{event?.location?.country}}</span>
+        </div>
+        <div *ngIf="event?.onlineUrl">URL: {{event?.onlineUrl}}</div>
     </div>
-    `
+    `,
+    styles:[
+        `.pad-left{ margin-left:10px; }`,
+        `.well div { color: #bbb; }`,
+        `.thumbnail { min-height: 210px}`
+    ]
 })
 export class EventThumbnailComponent {
     @Input() event:any;
