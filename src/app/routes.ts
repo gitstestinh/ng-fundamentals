34a952591379dd7df6dbe4c8682f1aefb1fdcefb
Evan Tolley <etolley@guthriegroup.com>
2018-10-29 20:54:26 -0400

    lazy loaded modules and first form

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -11,5 +11,6 @@ export const appRoutes: Routes = [
     { path: 'events', component: EventsListComponent, resolve:{ events:EventsListResolver}},
     { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator]},
     { path: '', redirectTo: 'events', pathMatch: 'full'},
+    { path: 'user', loadChildren: './user/user-module#UserModule'},
     { path: '404', component: Error404Component }
 ]
